2543c8c737171e41baba2a581e330bef31218b3b
ProgrammerShri <shrikantdewangan3@gmail.com>
2022-08-09 22:50:28 +0530

    style : dark theme added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import Dashboard from './pages/Dashboard'
+import Dashboard from "./pages/Dashboard";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#00bcd4",
+    },
+    secondary: {
+      main: "#ff4081",
+    },
+  },
+
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+  },
+
+});
 
 function App() {
-
   return (
-    <div className="App">
-      <div>
-        <Dashboard />
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <div>
+          <Dashboard />
+        </div>
       </div>
-    </div>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
